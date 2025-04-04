6a93d3b44706a0dd630bb8299615443a7b37962e
alanememichael9@gmail.com <alanememichael9@gmail.com>
2025-04-04 12:27:55 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import iconSuccess from "./assets/images/icon-success-check.svg"
 import './App.css'
 
 function App() {
@@ -10,7 +10,8 @@ function App() {
    event.preventDefault()
    const formElements = event.currentTarget
    const formData = new FormData(formElements) 
-   const names = formData.get('name').trim();
+   const fname = formData.get('fname').trim();
+   const lname = formData.get('lname').trim();
    const email = formData.get('email').trim();
    const queryType = formData.get('enquiry');
    const message = formData.get('message').trim()
@@ -18,55 +19,88 @@ function App() {
 
   
 
-   if(names === ''  || message === '')  {
-    
-    
+   if(consent && queryType && email.includes('@') && fname && lname   && message ) {
+
+    //i logging the following to the console to see if it is actually working
+   console.log(consent)
+   console.log(queryType)
+   console.log(email)
+   console.log(fname)
+   console.log(lname)
+   console.log(message)
+   event.currentTarget.reset()
+   
+   const successMessage = document.querySelector('.success-message')
+   
+   if(successMessage) {
+    successMessage.style.display = "block"
+   }
+
+   console.log(successMessage)
+
+   } else {
     event.preventDefault()
+   }
+ 
+   if(!fname) {
+    
+    alert('Please provide a first name')
 
    }
 
-   if(!email.includes('@')) {
+   if(!lname) {
 
-    alert('include a valid email');
-    event.preventDefault();
+    alert('Please provide a last name')
+
    }
 
    if(!queryType) {
 
-    alert('select grom radio')
-    event.preventDefault();
+    alert('Please select a query type ')
+
    }
 
    if(!consent) {
 
-    alert('consent to be contacted by the team')
-    event.preventDefault();
+    alert('Please consent the team to contact you')
+
    }
 
-   
+   if(!message) {
 
-   event.currentTarget.reset()
+    alert('Please send us a message')
 
-  }
+   }
+
+  } 
 
   
 
   return (
+
+    <>
+      <div className="success-message">
+        <header className='successHeader' ><img src={iconSuccess} alt="success logo" /><span style={{fontSize:"20px"}}>
+        Message sent!</span ></header>
+          <p style={{fontSize:"16px"}}>Thanks for completing the form, we will be in touch soon.</p>
+      </div>
+
     <main>
+
       <form onSubmit={contact} name='contact-form'>
 
         <section><h1>Contact us</h1></section>
 
         <section id='names'>
-          <div  className='positioning-error-message'>
+          <div  className='positioning-error-message' >
           <label htmlFor="fname">Firstname *</label> <br />
-          <input type="text" id='fname' name='name'/> 
-          
+          <input type="text" id='fname' name='fname'/> 
+      
           </div>
 
           <div className='positioning-error-message'>
           <label htmlFor="lname">Lastname *</label> <br />
-          <input type="text" id='lname' name='name' />
+          <input type="text" id='lname' name='lname' />
           
           </div> 
     
@@ -110,6 +144,7 @@ function App() {
 
       </form>
     </main>
+    </>
   )
 }
 
